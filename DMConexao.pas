eae48559f7500376cb4bc460b7b08fd6bfd6ecbb
jozielso <jozielso@gmail.com>
2022-05-30 20:53:40 -0400

    Commit_02

---
+++ DMConexao.pas
@@ -0,0 +1,32 @@
+unit DMConexao;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Data.DbxSqlite, FireDAC.Stan.Intf,
+  FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf,
+  FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys,
+  FireDAC.VCLUI.Wait, Data.DB, Data.Win.ADODB, FireDAC.Comp.Client, Data.SqlExpr;
+
+type
+  TConexao = class(TDataModule)
+    SQLConnection1: TSQLConnection;
+    FDConnection1: TFDConnection;
+    ADOConnection: TADOConnection;
+    ADOQuery: TADOQuery;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Conexao: TConexao;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
